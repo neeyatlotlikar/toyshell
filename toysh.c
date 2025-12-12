70aebabd5531ba60ce8aabdcc4041644c8a3a200
neeyatlotlikar <neeyat.lotlikar@gmail.com>
2025-12-12 16:34:04 +0530

    input parsed to argv format

--- toysh.c
+++ toysh.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#define MAX_TOKENS 128
 
 int main(void) {
 	char *line = NULL;
@@ -16,7 +20,28 @@ int main(void) {
 			break;
 		}
 
-		printf("you typed: %s", line);
+		if (nread > 0 && line[nread - 1] == '\n') {
+			line[nread - 1] = '\0';
+		}
+
+		char *argv[MAX_TOKENS];
+		int argc = 0;
+
+		char *token = strtok(line, " \t");
+		while (token != NULL && argc < MAX_TOKENS - 1) {
+			argv[argc++] = token;
+			token = strtok(NULL, " \t");
+		}
+		argv[argc] = NULL;
+
+		if (argc == 0) {
+			continue;
+		}
+
+		printf("argv[0] = %s\n", argv[0]);
+		for (int i = 1; i < argc; i++) {
+			printf("argv[%d] = %s\n", i, argv[i]);
+		}
 	}
 
 	free(line);
